233e2ac175d6e1b91359a08ed483f2ef30e2505a
letsgetrusty <bogdan@letsgetrusty.com>
2024-06-24 18:05:46 +0200

    Add charts

---
+++ charts/chart-1.js
@@ -0,0 +1,29 @@
+const ctx = document.getElementById('chart-1');
+
+new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    responsive: true,
+      plugins: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: false
+      },
+    }
+  }
+});
