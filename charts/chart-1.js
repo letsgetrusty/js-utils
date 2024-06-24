2f21854dee2f41fe1f74dec5cae5bf3758539ac5
letsgetrusty <bogdan@letsgetrusty.com>
2024-06-24 23:08:37 +0200

    Update legned position

--- charts/chart-1.js
+++ charts/chart-1.js
@@ -1,29 +1,32 @@
 const ctx = document.getElementById('chart-1');
 
 new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+            {
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+           }
+        ]
     },
-    responsive: true,
-      plugins: {
-      legend: {
-        display: false
-      },
-      title: {
-        display: false
-      },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        responsive: true,
+        plugins: {
+            legend: {
+                // display: false
+                position: 'chartArea'
+            },
+            title: {
+                display: false
+            },
+        }
     }
-  }
 });
