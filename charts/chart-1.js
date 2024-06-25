1756945c41d20a769e5eab1e4291db5e2302c3a4
letsgetrusty <bogdan@letsgetrusty.com>
2024-06-25 13:05:09 +0200

    Adjustable height

--- charts/chart-1.js
+++ charts/chart-1.js
@@ -3,32 +3,37 @@ const ctx = document.getElementById('chart-1');
 const datasets = [{
         'label': 'AI/ML',
         'color': 'red',
-        'value': 12
+        'value': 12.00
     },
     {
         'label': 'Backend',
         'color': 'blue',
-        'value': 19
+        'value': 19.02
     },
     {
         'label': 'Embedded/low-level',
         'color': 'green',
-        'value': 3
+        'value': 45.55
     },
     {
         'label': 'Finance',
         'color': 'orange',
-        'value': 5
+        'value': 5.23
     },
     {
         'label': 'Blockchain/Web3',
         'color': 'purple',
-        'value': 2
+        'value': 26.52
     },
     {
         'label': 'Desktop/mobile',
         'color': 'yellow',
-        'value': 3
+        'value': 3.25
+    },
+    {
+        'label': 'Other',
+        'color': 'grey',
+        'value': 1.51
     }
 ];
 
@@ -36,13 +41,23 @@ var labels = datasets.map(data => data.label);
 var colors = datasets.map(data => data.color);
 var values = datasets.map(data => data.value);
 
+var percentFormatter = (value, ctx) => {
+    let sum = 0;
+    let dataArr = ctx.chart.data.datasets[0].data;
+    dataArr.map(data => {
+        sum += data;
+    });
+    let percentage = (value*100 / sum).toFixed(2)+"%";
+    return percentage;
+};
+
 new Chart(ctx, {
     type: 'doughnut',
     data: {
         labels: labels,
         datasets: [
             {
-                label: 'market share',
+                label: 'market share %',
                 data: values,
                 borderWidth: 1
            }
@@ -50,13 +65,17 @@ new Chart(ctx, {
     },
     options: {
         responsive: true,
+        maintainAspectRatio: false,
         plugins: {
             legend: {
-                position: 'bottom'
+                position: 'left'
             },
             title: {
                 display: true
             },
+            datalabels: {
+                formatter: percentFormatter
+            }
         }
     }
 });
